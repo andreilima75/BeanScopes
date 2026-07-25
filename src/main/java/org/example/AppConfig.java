40fa0d5d8275819a5b80d930be29109132ae3fc7
Andrei Lima <andrei.lima75@gmail.com>
2026-07-25 20:44:32 -0300

    Bean Scopes

--- src/main/java/org/example/AppConfig.java
+++ src/main/java/org/example/AppConfig.java
@@ -1,9 +1,21 @@
 package org.example;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ComponentScan(basePackages = "com.example")
 public class AppConfig {
+
+    @Bean
+    @Scope("singleton")
+    public SingletonService singletonService() {
+        return new SingletonService();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public PrototypeService prototypeService() {
+        return new PrototypeService();
+    }
 }
